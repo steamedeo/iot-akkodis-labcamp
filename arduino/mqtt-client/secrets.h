6ee4f2691c0e5008dbe28c68c603e6afe3a00d56
Stefano <ste.amedeo@gmail.com>
2023-03-04 23:28:38 +0100

    create client to send temperature and light json telemetry

---
+++ arduino/mqtt-client/secrets.h
@@ -0,0 +1,3 @@
+static const char WIFI_SSID[] = "<wifi ssid>";
+static const char WIFI_PASSWORD[] = "<wifi password>";
+static const char IOT_HUB_CONNECTION_STRING[] = "<iot hub connection string>";
--- arduino/secrets.h
+++
@@ -1,3 +0,0 @@
-static const char WIFI_SSID[] = "<wifi ssid>";
-static const char WIFI_PASSWORD[] = "<wifi password>";
-static const char IOT_HUB_CONNECTION_STRING[] = "<iot hub connection string>";
